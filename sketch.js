4f5e1373a92aa4598e731c2df6f50fca71a3bc4a
Marte0 <matteodellagostino@gmail.com>
2025-01-02 17:36:36 +0100

    MIAO

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let value = 0;
+let threshold = 10;
+
+function setup() {
+  setShakeThreshold(threshold);
+}
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+  describe(`50-by-50 black rect in center of canvas.
+    turns white on mobile when device is being shaked`);
+}
+function deviceMoved() {
+  value = value + 5;
+  threshold = threshold + 5;
+  if (value > 255) {
+    value = 0;
+    threshold = 30;
+  }
+  setShakeThreshold(threshold);
+}
+
+function touchEnded() {
+  if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission) {
+    DeviceOrientationEvent.requestPermission();
+  }
+}
