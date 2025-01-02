838d107415b43bfb6190a8e69a4614a8c8060a8a
Marte0 <matteodellagostino@gmail.com>
2025-01-02 18:28:26 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function draw() {
   if (frameCount % 4 == 0) {
     totalShake = totalShake + shakeStrength;
   }
-  background(20, 200, 0);
+  background(100, 200, 0);
   text(`Shake Strength tot2: ${totalShake.toFixed(2)}`, width / 2, height / 2);
 }
 
